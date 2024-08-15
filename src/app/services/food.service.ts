396d2b99006948eff6b57421d3fa7fb5f77899cb
M-Erayman <muhammedderayman@gmail.com>
2024-08-15 17:48:16 +0300

    at last, tag components added

---
+++ src/app/services/food.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Food } from '../shared/models/food';
+import { sample_foods, sample_tags } from '../../data';
+import { Tag } from '../shared/models/tag';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoodService {
+  constructor() {}
+
+  getAll(): Food[] {
+    return sample_foods;
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: String) {
+    return this.getAll().filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  getAllTags(): Tag[] {
+    return sample_tags;
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    if (tag === 'All') {
+      return this.getAll();
+    } else {
+      return this.getAll().filter((food) => food.tags?.includes(tag));
+    }
+  }
+}
