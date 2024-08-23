712da8de0ecf86fb0ef7fccc5d9064d1802c79e8
M-Erayman <muhammedderayman@gmail.com>
2024-08-23 14:30:21 +0300

    login added

--- src/app/components/partials/input-container/input-container.component.ts
+++ src/app/components/partials/input-container/input-container.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-input-container',
+  selector: 'input-container',
   templateUrl: './input-container.component.html',
-  styleUrl: './input-container.component.css'
+  styleUrl: './input-container.component.css',
 })
 export class InputContainerComponent {
-
+  @Input() label!: string;
+  @Input() bgColor = 'white';
 }
